8083844fe31a10da642d0fc58add25fcdc65c85b
mrbinay-dot <nrnay166@gmail.com>
2026-02-09 13:35:44 +0545

    added input city name fuuction to check city wheather

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const apiKey = "87ccd64ad7bc5a6cec27f1852b49a1b0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const reponse = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+
+async function checkWeather(city) {
+    const reponse = await fetch(apiUrl + city+ `&appid=${apiKey}`);
     var data = await reponse.json();
 
     console.log(data);
 
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.floor(data.main.temp)+  "°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+" km/h";
+
 
     
 }
-   checkWeather( );
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather( searchBox.value);
+
+})
+    
